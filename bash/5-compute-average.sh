16c6ce84dcd38ffc28574322df2f8d43f3ec17f3
Felix Schaipp <felix.schaipp@gmail.com>
2021-01-22 20:51:06 +0100

    Bash challenge description #5

---
+++ bash/5-compute-average.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+Given N integers, compute their average, rounded to three decimal places.
+
+Input Format
+The first line contains an integer, N.
+Each of the following N lines contains a single integer.
+
+Output Format
+Display the average of the  integers, rounded off to three decimal places.
