0f8a2f91b501ed20f96f5571ddf8948064e8761f
Felix Schaipp <felix.schaipp@gmail.com>
2022-01-11 21:14:57 +0100

    feat: use the generator with user input

--- sento/intersecting-rectangles/src/main.py
+++ sento/intersecting-rectangles/src/main.py
@@ -1,2 +1,15 @@
+from classes.rectanglePairJSONGenerator import RectanglePairJSONGenerator
+
 if __name__ == "__main__":
-    print('First test')
+    pairsToGenerate = int(input("How many rectangles should be created?: "))
+    if(pairsToGenerate < 1):
+        print(print("Please enter a number bigger than 0"))
+        exit(1)
+    generator = RectanglePairJSONGenerator(pairsToGenerate)
+    generator.generatePairs()
+    generator.saveToJson()
+    print(f"We created a json file (rectangle-pairs.json) with {pairsToGenerate} pairs of rectangles for you...")
+    
+    # read in the generated json
+    # let a checker class run through to the pairs
+    # pretty print the output of the check
